4cbf8d42ab4ce648cb74f72d5db787d324d87c13
vijun <vijun.testautomation@gmail.com>
2016-04-22 00:56:35 +0530

    Initial update of git project.

---
+++ com.GitProject.selenium/src/test/java/maven_FirstPackage/SamTest.java
@@ -0,0 +1,12 @@
+package maven_FirstPackage;
+
+import org.testng.annotations.Test;
+
+public class SamTest {
+	@Test
+	public void verifyTest(){
+		System.out.println("This is my first maven project");
+		
+	}
+
+}
