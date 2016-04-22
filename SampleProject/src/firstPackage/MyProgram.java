74ac54ab2f95738c0be2d1b4912922a2263fbaea
vijun <vijun.testautomation@gmail.com>
2016-04-22 09:39:53 +0530

    Output changed.

--- SampleProject/src/firstPackage/MyProgram.java
+++ SampleProject/src/firstPackage/MyProgram.java
@@ -5,6 +5,7 @@ public class MyProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hi.. vijun");
+		System.out.println("Hello github");
 	}
 
 }
